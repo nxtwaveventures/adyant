b88c97d0df40fb69524d5a6988743d11f0f657b1
Bhupinder Jassi <bjiitd@gmail.com>
2025-04-06 15:46:33 +0530

    Fix: Update basePath and assetPrefix to match correct repository name

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   output: 'export',
   // Get the repository name from env or use default name
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/adyant_webpage',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '/adyant_webpage',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '/adyant',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '/adyant',
 };
 
 export default nextConfig;
